8b0ac9f297f5ef00e7903c47e20b2d2e8776d583
Marco Gelli <marcogelli1994@gmail.com>
2024-01-28 16:55:56 +0100

    Added register page

---
+++ app/register/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import RegisterFormPrivate from "@/components/RegisterFormPrivate";
+import RegisterStepperPrivate from "@/components/RegisterStepperPrivate";
+import { Separator } from "@/components/ui/separator";
+import { UserPlus2Icon } from "lucide-react";
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [step, setStep] = useState(1);
+
+  return (
+    <>
+      <Tabs defaultValue="private">
+        <div className="flex min-h-screen flex-col bg-gray-50 sm:flex-row">
+          {/* Left Panel */}
+          <div className="sm:p-18 flex w-full flex-col items-center justify-center bg-gradient-to-br from-orange-400 to-rose-600 p-14 text-white sm:w-2/5">
+            <h1 className="mb-4 text-5xl font-bold sm:text-6xl">Sign Up</h1>
+            <p className="py-2">as</p>
+            <TabsList className="w-full">
+              <TabsTrigger
+                value="private"
+                className="w-full justify-center data-[state=active]:bg-primary data-[state=active]:text-white "
+              >
+                Private
+              </TabsTrigger>
+              <TabsTrigger
+                value="agent"
+                className="w-full justify-center data-[state=active]:bg-primary data-[state=active]:text-white "
+              >
+                Agent
+              </TabsTrigger>
+            </TabsList>
+            <Image
+              src="logo-white.svg"
+              alt="Signup cover image"
+              width={150}
+              height={150}
+              className="my-4"
+            />
+            <h3 className="text-xl font-semibold">Urban Hunt</h3>
+            <h4 className="italic">«Join the community of hunters»</h4>
+            <Separator className="mt-6" />
+            <div className="mt-4 flex items-center justify-center">
+              <span className="text-md mr-2 sm:text-base">
+                Already have an account?
+              </span>
+              <Link href="/login">
+                <Button
+                  variant="link"
+                  className="text-md text-white sm:text-base"
+                >
+                  Login
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center">
+            <RegisterStepperPrivate step={step} />
+          </div>
+          {/* Right Panel */}
+          <div className="flex flex-1 flex-col justify-center">
+            <div className="mx-auto flex w-full max-w-md flex-col items-center rounded-lg border border-gray-300 bg-white p-6 shadow-lg">
+              <div className="mx-auto w-full max-w-md">
+                <div className="flex justify-center">
+                  <UserPlus2Icon size={100} className="mb-4 text-primary" />
+                </div>
+                <TabsContent value="private" className="w-full">
+                  <RegisterFormPrivate step={step} setStep={setStep} />
+                </TabsContent>
+                <TabsContent value="agent" className="w-full">
+                  <RegisterFormPrivate step={step} setStep={setStep} />
+                </TabsContent>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Tabs>
+    </>
+  );
+}
---
+++ components/RegisterStepperPrivate.tsx
@@ -0,0 +1,53 @@
+import {
+  HeartHandshakeIcon,
+  KeyRoundIcon,
+  PhoneIcon,
+  UserSquare2Icon,
+} from "lucide-react";
+
+interface RegisterStepperPrivateProps {
+  step: number;
+}
+
+export default function RegisterStepperPrivate({
+  step,
+}: RegisterStepperPrivateProps) {
+  function textClass(stepNumber: number) {
+    return `font-medium leading-tight ${
+      step === stepNumber ? "text-orange-500" : "text-gray-500"
+    }`;
+  }
+
+  function iconBg(stepNumber: number) {
+    return step === stepNumber
+      ? "bg-orange-500 text-white"
+      : "bg-gray-100 text-gray-500";
+  }
+
+  return (
+    <>
+      <ol className="relative border-l border-gray-200 dark:border-gray-700">
+        {[
+          { Icon: KeyRoundIcon, text: "1. Access credentials" },
+          { Icon: UserSquare2Icon, text: "2. Personal info" },
+          { Icon: PhoneIcon, text: "3. Contacts" },
+          { Icon: HeartHandshakeIcon, text: "4. Terms & conditions" },
+        ].map((item, index) => (
+          <li key={index} className={`mb-10 ml-6 ${textClass(index + 1)}`}>
+            <div className="flex items-center">
+              <span
+                className={`absolute -left-4 flex h-8 w-8 items-center justify-center rounded-full ring-4 ring-white dark:ring-gray-900 ${iconBg(
+                  index + 1
+                )}`}
+              >
+                <item.Icon className="h-5 w-5" />
+              </span>
+
+              <h3 className="ml-2">{item.text}</h3>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
