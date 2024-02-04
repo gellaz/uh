894c4be9b79cf878e290de806a2eb5a5ec931fef
Marco Gelli <marcogelli1994@gmail.com>
2024-02-04 17:09:25 +0100

    fix: added missing package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
-'use client'
+"use client";
 
-import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import { usePathname } from "next/navigation";
-import { Inter } from 'next/font/google'
-import SideBar from '@/components/sideBar'
-import AppBar from '@/components/AppBar'
 
+import AppBar from "@/components/AppBar";
 import { GeistSans } from "geist/font/sans";
+import { Inter } from "next/font/google";
+import SideBar from "@/components/sideBar";
+import { usePathname } from "next/navigation";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata = {
 //   metadataBase: new URL(defaultUrl),
@@ -20,35 +19,36 @@ const inter = Inter({ subsets: ['latin'] })
 //   description: "The fastest way to build apps with Next.js and Supabase",
 // };
 
-export default function RootLayout({ children, }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const pathname = usePathname();
   const hideAppBar = () => {
-    const hasNew = pathname.includes('/new')
-    const hasMesCenter = pathname.includes('/message_center')
-    return (hasNew === hasMesCenter)
-  }
+    const hasNew = pathname.includes("/new");
+    const hasMesCenter = pathname.includes("/message_center");
+    return hasNew === hasMesCenter;
+  };
   const hideSideBar = () => {
-    const hasDetail = pathname.includes('/detail');
-    const hasNew = pathname.includes('/new')
-    const hasMesCenter = pathname.includes('/message_center')
-    const hasExplore = pathname.includes('/explore')
-    return (hasDetail || hasNew || hasMesCenter || hasExplore)
-  }
+    const hasDetail = pathname.includes("/detail");
+    const hasNew = pathname.includes("/new");
+    const hasMesCenter = pathname.includes("/message_center");
+    const hasExplore = pathname.includes("/explore");
+    return hasDetail || hasNew || hasMesCenter || hasExplore;
+  };
 
   return (
     <html lang="it" className={GeistSans.className}>
-      <body className={inter.className + ' flex flex-row w-screen h-screen'}>
-        {
-          hideSideBar() ? null : <SideBar />
-        }
-        <div className={'flex flex-col ' + (hideSideBar() ? 'w-full' : 'w-[94%]')}>
-          {
-            hideAppBar() ? <AppBar /> : null
-          }
+      <body className={inter.className + " flex flex-row w-screen h-screen"}>
+        {hideSideBar() ? null : <SideBar />}
+        <div
+          className={"flex flex-col " + (hideSideBar() ? "w-full" : "w-[94%]")}
+        >
+          {hideAppBar() ? <AppBar /> : null}
           {children}
         </div>
       </body>
     </html>
   );
 }
-
