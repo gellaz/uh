f694c1cd5d83e7463a70fd59bf2cdf38f6bf7c36
Marco Gelli <marcogelli1994@gmail.com>
2024-02-04 18:47:21 +0100

    Using different layouts and folder tree modified

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,42 @@
+import AppBar from "@/components/AppBar";
+import SideBar from "@/components/sideBar";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+  /*   const pathname = usePathname();
+  const hideAppBar = () => {
+    const hasNew = pathname.includes("/new");
+    const hasMesCenter = pathname.includes("/message_center");
+    return hasNew === hasMesCenter;
+  };
+  const hideSideBar = () => {
+    const hasDetail = pathname.includes("/detail");
+    const hasNew = pathname.includes("/new");
+    const hasMesCenter = pathname.includes("/message_center");
+    const hasExplore = pathname.includes("/explore");
+    return hasDetail || hasNew || hasMesCenter || hasExplore;
+  }; */
+
+  return (
+    <main className="flex flex-row h-screen w-screen">
+      <SideBar />
+      <div className={"flex flex-col w-full"}>
+        <AppBar />
+        {children}
+      </div>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,36 @@
-"use client";
-
 import "./globals.css";
 
-import AppBar from "@/components/AppBar";
 import { GeistSans } from "geist/font/sans";
-import { Inter } from "next/font/google";
-import SideBar from "@/components/sideBar";
-import { usePathname } from "next/navigation";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
-const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   metadataBase: new URL(defaultUrl),
-//   title: "Next.js and Supabase Starter Kit",
-//   description: "The fastest way to build apps with Next.js and Supabase",
-// };
+/* export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+}; */
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const hideAppBar = () => {
-    const hasNew = pathname.includes("/new");
-    const hasMesCenter = pathname.includes("/message_center");
-    return hasNew === hasMesCenter;
-  };
-  const hideSideBar = () => {
-    const hasDetail = pathname.includes("/detail");
-    const hasNew = pathname.includes("/new");
-    const hasMesCenter = pathname.includes("/message_center");
-    const hasExplore = pathname.includes("/explore");
-    return hasDetail || hasNew || hasMesCenter || hasExplore;
-  };
+  /*   const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/");
+  } */
   return (
-    <html lang="it" className={GeistSans.className}>
-      <body className={inter.className + " flex flex-row w-screen h-screen"}>
-        {hideSideBar() ? null : <SideBar />}
-        <div
-          className={"flex flex-col " + (hideSideBar() ? "w-full" : "w-[94%]")}
-        >
-          {hideAppBar() ? <AppBar /> : null}
-          {children}
-        </div>
+    <html lang="en" className={GeistSans.className}>
+      <body className="">
+        <main className="">{children}</main>
       </body>
     </html>
   );
