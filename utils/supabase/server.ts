5fb84ec85ca7c8d2bdcbfbf821f98f1da6c36c54
Marco Gelli <marcogelli1994@gmail.com>
2024-02-07 01:17:02 +0100

    Refactor Supabase client creation adding typing with database generated types

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,8 +1,9 @@
+import { Database } from "@/types/supabase";
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
 export function createClient(cookieStore: ReturnType<typeof cookies>) {
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
