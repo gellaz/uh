9dc3e36ee51e58e37bce50727fb7b581e06f01f8
Marco Gelli <marcogelli1994@gmail.com>
2024-02-26 01:07:07 +0100

    Update global.d.ts with new type definitions

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -5,22 +5,50 @@ import type { Database as DB } from "@/lib/database.types";
  */
 declare global {
   export type Database = DB;
+
   export type Profile = DB["public"]["Tables"]["profiles"]["Row"];
-  export type Property = DB["public"]["Tables"]["properties"]["Row"];
+  export type Sex = DB["public"]["Enums"]["sex"];
+
   export type PropertyCategory =
     DB["public"]["Tables"]["property_categories"]["Row"];
-  export type AirConditioning = DB["public"]["Enums"]["air_conditioning"];
-  export type Condition = DB["public"]["Enums"]["condition"];
-  export type Elevator = DB["public"]["Enums"]["elevator"];
-  export type EnergyClass = DB["public"]["Enums"]["energy_class"];
-  export type Exposure = DB["public"]["Enums"]["exposure"];
-  export type ExternalFixturesGlassType =
-    DB["public"]["Enums"]["external_fixtures_glass_type"];
-  export type ExternalFixturesMaterial =
-    DB["public"]["Enums"]["external_fixtures_material"];
-  export type Furnishing = DB["public"]["Enums"]["furnishing"];
-  export type Heating = DB["public"]["Enums"]["heating"];
-  export type HeatingFuel = DB["public"]["Enums"]["heating_fuel"];
-  export type HeatingType = DB["public"]["Enums"]["heating_type"];
-  export type Sex = DB["public"]["Enums"]["sex"];
+  export type PropertyResidential =
+    DB["public"]["Tables"]["properties_residential"]["Row"];
+  export type PropertyResidentialSubcategory =
+    DB["public"]["Enums"]["property_residential_subcategory_enum"];
+
+  export type PropertyAirConditioning =
+    DB["public"]["Enums"]["property_air_conditioning_enum"];
+  export type PropertyAirConditioningType =
+    DB["public"]["Enums"]["property_air_conditioning_type_enum"];
+  export type PropertyCadestralCategory =
+    DB["public"]["Enums"]["property_cadestral_category_enum"];
+  export type PropertyClass = DB["public"]["Enums"]["property_class_enum"];
+  export type PropertyConciergeService =
+    DB["public"]["Enums"]["property_concierge_service_enum"];
+  export type PropertyCondition =
+    DB["public"]["Enums"]["property_condition_enum"];
+  export type PropertyCountry = DB["public"]["Enums"]["property_country_enum"];
+  export type PropertyEnergyClass =
+    DB["public"]["Enums"]["property_energy_class_enum"];
+  export type PropertyExposure =
+    DB["public"]["Enums"]["property_exposure_enum"];
+  export type PropertyExternalFixturesGlassType =
+    DB["public"]["Enums"]["property_external_fixtures_glass_type_enum"];
+  export type PropertyExternalFixturesMaterial =
+    DB["public"]["Enums"]["property_external_fixtures_material_enum"];
+  export type PropertyFacing = DB["public"]["Enums"]["property_facing_enum"];
+  export type PropertyFloor = DB["public"]["Enums"]["property_floor_enum"];
+  export type PropertyFreeSides =
+    DB["public"]["Enums"]["property_free_sides_enum"];
+  export type PropertyFurnishingFurnishing =
+    DB["public"]["Enums"]["property_furnishing_enum"];
+  export type PropertyGarage = DB["public"]["Enums"]["property_garage_enum"];
+  export type PropertyGarden = DB["public"]["Enums"]["property_garden_enum"];
+  export type PropertyHeating = DB["public"]["Enums"]["property_heating_enum"];
+  export type PropertyHeatingFuel =
+    DB["public"]["Enums"]["property_heating_fuel_enum"];
+  export type PropertyHeatingType =
+    DB["public"]["Enums"]["property_heating_type_enum"];
+  export type PropertyTVSystem =
+    DB["public"]["Enums"]["property_tv_system_enum"];
 }
