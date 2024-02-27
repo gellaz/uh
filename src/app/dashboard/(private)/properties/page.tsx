195a5f30f5fd38a268fabd9299fbd41e7a796bdd
Marco Gelli <marcogelli1994@gmail.com>
2024-02-27 22:08:20 +0100

    Add button to add new property on DashboardPropertiesPage

--- src/app/dashboard/(private)/properties/page.tsx
+++ src/app/dashboard/(private)/properties/page.tsx
@@ -1,8 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Icon } from "@iconify-icon/react";
+import Link from "next/link";
+
 export default function DashboardPropertiesPage() {
   return (
-    <div>
-      <h1 className="font-extrabold text-3xl mb-2">Properties</h1>
-      <p className="text-sm text-pretty">Browse your properties</p>
+    <div className="flex flex-col">
+      <div className="flex justify-between items-center py-3 px-4">
+        <div className="flex flex-col">
+          <h1 className="font-extrabold text-3xl mb-2">Properties</h1>
+        </div>
+        <Button asChild>
+          <Link href="/dashboard/properties/new">
+            <Icon
+              icon={"material-symbols:add"}
+              height={20}
+              width={20}
+              className="mr-2"
+            />
+            Add Property
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 }
