3cf02f91664ccacb8950b156b442be1200e676ba
Francesco Limoni <limoni.francesco21@gmail.com>
2024-03-03 16:12:10 +0100

    Add VerticalStepper component and update New residential property page

--- src/app/dashboard/(private)/properties/new/residential/page.tsx
+++ src/app/dashboard/(private)/properties/new/residential/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 
+import VerticalStepper from "@/components/dashboard/property/new/VerticalStepper";
 import { formSteps } from "@/constants/newPropertyResidentialSteps";
+import { FormStepProvider } from "@/context/FormStepContext";
 import { ChevronRight } from "lucide-react";
 import { useState } from "react";
 
 
-export default async function NewPropertyPage() {
+export default function NewPropertyPage() {
   const [step, setStep] = useState(0);
-  
-  const liStyle = "row px-2 py-4 h-fit justify-between items-center hover:bg-orange-50 border-b-[1px] ";
-  const liStyleActive = "bg-white hover:bg-orange-50";
+
 
   return (
     <div className="col space-y-4">
@@ -23,24 +23,10 @@ export default async function NewPropertyPage() {
       {/* LEFT SIDE */}
       <div className="row w-full h-full space-x-4">
         <div className="col w-[28%]">
-          {/* <ol>
-            {
-              formSteps.map((item, index) => {
-                return (
-                  <li key={item.title}
-                    onSelect={() => setStep(index)}
-                    className={liStyle + (index === step ? liStyleActive : null)}>
-                    <p className={(index === step ? "text-primary" : "text-black")}>{item.title}</p>
-                    <div className="row w-fit space-x-1 opacity-50">
-                      <p className={"text-xs font-medium" + getStatusColor(item)}>{getStatus(item)}</p>
-                      <ChevronRight size={16} />
-                    </div>
-                  </li>
-                )
-              }
-              )
-            }
-          </ol> */}
+          <FormStepProvider steps={formSteps}>
+            <VerticalStepper />
+          </FormStepProvider>
+
         </div>
 
         {/* RIGHT SIDE */}
