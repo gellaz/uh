8f89586684b41d33f661e959e9ea6ec818c8c5d5
Marco Gelli <marcogelli1994@gmail.com>
2024-03-04 01:28:27 +0100

    Refactor VerticalStepper component and add ListItem component

--- src/components/dashboard/property/new/VerticalStepper.tsx
+++ src/components/dashboard/property/new/VerticalStepper.tsx
@@ -1,49 +1,64 @@
 "use client";
 
+import { FormStep, useFormStep } from "@/context/FormStepContext";
+
 import { ChevronRight } from "lucide-react";
 import React from "react";
-import { useFormStep } from "@/context/FormStepContext";
+import { Separator } from "@radix-ui/react-separator";
 
-enum VerticalStepperItemStatus {
-  PENDING,
-  COMPLETED,
-  ERROR,
+interface ListItemProps {
+  item: FormStep;
+  index: number;
+  isActive: boolean;
+  goToStep: (index: number) => void;
 }
 
-interface VerticalStepperItem {
-  title: string;
-  status: VerticalStepperItemStatus;
+function ListItem({ item, index, isActive, goToStep }: ListItemProps) {
+  return (
+    <li
+      key={item.title}
+      onClick={() => goToStep(index)}
+      className={`row px-4 py-4 h-fit justify-between items-center hover:bg-primary/10 hover:cursor-pointer border-b-[1px] rounded-lg shadow-md ${
+        isActive && "bg-white ring-2 ring-primary"
+      }`}
+    >
+      <div className="flex items-center">
+        <span
+          className={`mr-2 font-bold text-md text-black rounded-full w-8 h-8 flex items-center justify-center border ${
+            isActive && "border-primary"
+          }`}
+        >
+          {index + 1}
+        </span>
+        <Separator orientation="vertical" />
+        <p
+          className={isActive ? "font-semibold text-primary" : "text-slate-400"}
+        >
+          {item.title}
+        </p>
+      </div>
+      <div className="row w-fit space-x-1 opacity-50">
+        <p className={"text-xs font-medium"}>{item.description}</p>
+        <ChevronRight size={16} />
+      </div>
+    </li>
+  );
 }
 
 export default function VerticalStepper() {
   const { steps, currentStepIndex, goToStep } = useFormStep();
-  const liStyle =
-    "row px-2 py-4 h-fit justify-between items-center hover:bg-orange-50 hover:cursor-pointer border-b-[1px] ";
-  const liStyleActive = "bg-white hover:bg-orange-50";
 
   return (
-    <ol>
+    <ol className="space-y-2">
       {steps.map((item, index) => {
         const isActive = index === currentStepIndex;
-        console.log("currentStepIndex", currentStepIndex);
-
         return (
-          <li
-            key={item.title}
-            onClick={() => goToStep(index)}
-            className={liStyle + (isActive ? liStyleActive : null)}
-          >
-            <p className={isActive ? "text-primary" : "text-black"}>
-              {item.title}
-            </p>
-            <div className="row w-fit space-x-1 opacity-50">
-              <p className={"text-xs font-medium"}>
-                {/* //+ getStatusColor(item) */}
-                {item.description}
-              </p>
-              <ChevronRight size={16} />
-            </div>
-          </li>
+          <ListItem
+            item={item}
+            index={index}
+            isActive={isActive}
+            goToStep={goToStep}
+          />
         );
       })}
     </ol>
